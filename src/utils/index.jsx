c650cb4e6864868887135729fffbc45b733c4610
dhanrajMohan <dhanraj.mohanraj@giantconnect.com>
2022-11-05 18:34:13 +0530

    Code cleanup.

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -1,37 +1,5 @@
 import { metaMask } from "../connectors/Metamask";
 
-export function getTransactionReceiptMined(txHash, library, interval) {
-  const transactionReceiptAsync = (resolve, reject) => {
-    library
-      .getTransactionReceipt(txHash)
-      .then((receipt) => {
-        if (receipt == null) {
-          setTimeout(
-            () => transactionReceiptAsync(resolve, reject),
-            interval ? interval : 500
-          );
-        } else {
-          resolve(receipt);
-        }
-      })
-      .catch((e) => {
-        console.log("Error getting transaction receipt--->", e);
-      });
-  };
-
-  if (Array.isArray(txHash)) {
-    return Promise.all(
-      txHash.map((oneTxHash) =>
-        getTransactionReceiptMined(oneTxHash, library, interval)
-      )
-    );
-  } else if (typeof txHash === "string") {
-    return new Promise(transactionReceiptAsync);
-  } else {
-    throw new Error("Invalid Type: " + txHash);
-  }
-}
-
 export const handleConnect = () => {
   if (window && window?.ethereum) {
     metaMask.activate();
