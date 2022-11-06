3f4b4be64148a11847008baf0873ae95bbb1f210
dhanrajMohan <dhanraj.mohanraj@giantconnect.com>
2022-11-06 13:58:46 +0530

    No data plans condition updated.

--- src/views/landingpage/index.jsx
+++ src/views/landingpage/index.jsx
@@ -59,63 +59,57 @@ const LandingPage = ({
       <S.LandingPageContainer>
         <S.MyDataPlansCard>
           <S.MyDataPlansCardHeader>My Data Plans</S.MyDataPlansCardHeader>
-          {activePlans.length > 0 && inActivePlans.length > 0 ? (
-            <>
-              {plansLoading ? (
-                <S.ConnectWalletButtonContainer>
-                  <CircularProgress
-                    sx={{ postion: "relative", top: "-2rem" }}
-                  />
-                </S.ConnectWalletButtonContainer>
-              ) : (
-                <>
-                  {account !== undefined ? (
-                    <S.MyDataPlansCardBody>
-                      {inActivePlans?.map((data, index) => {
-                        return (
-                          <MyDataPlanCard
-                            data={data}
-                            key={index}
-                            adminAddress={adminAddress}
-                            status="Inactive"
-                            setUpdateUserPlans={() => setUpdateUserPlans(true)}
-                            setEnableShowQRModal={setEnableShowQRModal}
-                            setModalData={setModalData}
-                          />
-                        );
-                      })}
-                      {activePlans?.map((data, index) => {
-                        return (
-                          <MyDataPlanCard
-                            data={data}
-                            key={data.id}
-                            status="Active"
-                            setEnableShowQRModal={setEnableShowQRModal}
-                            setModalData={setModalData}
-                          />
-                        );
-                      })}
-                    </S.MyDataPlansCardBody>
-                  ) : (
-                    <S.ConnectWalletButtonContainer>
-                      <S.ConnectWalletButton onClick={handleConnect}>
-                        <img src={MetamaskIcon} alt="metamask icon" />
-                        Connect your Metamask wallet
-                      </S.ConnectWalletButton>
-                    </S.ConnectWalletButtonContainer>
-                  )}
-                </>
-              )}
-            </>
-          ) : (
-            <S.ConnectWalletButtonContainer
-              sx={{ position: "relative", top: "-2rem" }}
-            >
-              <S.NoPlansText>
-                No data plans owned. Checkout our data plans collection below
-              </S.NoPlansText>
-            </S.ConnectWalletButtonContainer>
-          )}
+          <>
+            {plansLoading ? (
+              <S.ConnectWalletButtonContainer>
+                <CircularProgress sx={{ postion: "relative", top: "-2rem" }} />
+              </S.ConnectWalletButtonContainer>
+            ) : activePlans.length <= 0 && inActivePlans.length <= 0 ? (
+              <S.ConnectWalletButtonContainer>
+                <S.NoPlansText sx={{ postion: "relative", top: "-2rem" }}>
+                  No data plans owned. Checkout our data plans collection below
+                </S.NoPlansText>
+              </S.ConnectWalletButtonContainer>
+            ) : (
+              <>
+                {account !== undefined ? (
+                  <S.MyDataPlansCardBody>
+                    {inActivePlans?.map((data, index) => {
+                      return (
+                        <MyDataPlanCard
+                          data={data}
+                          key={index}
+                          adminAddress={adminAddress}
+                          status="Inactive"
+                          setUpdateUserPlans={() => setUpdateUserPlans(true)}
+                          setEnableShowQRModal={setEnableShowQRModal}
+                          setModalData={setModalData}
+                        />
+                      );
+                    })}
+                    {activePlans?.map((data, index) => {
+                      return (
+                        <MyDataPlanCard
+                          data={data}
+                          key={data.id}
+                          status="Active"
+                          setEnableShowQRModal={setEnableShowQRModal}
+                          setModalData={setModalData}
+                        />
+                      );
+                    })}
+                  </S.MyDataPlansCardBody>
+                ) : (
+                  <S.ConnectWalletButtonContainer>
+                    <S.ConnectWalletButton onClick={handleConnect}>
+                      <img src={MetamaskIcon} alt="metamask icon" />
+                      Connect your Metamask wallet
+                    </S.ConnectWalletButton>
+                  </S.ConnectWalletButtonContainer>
+                )}
+              </>
+            )}
+          </>
         </S.MyDataPlansCard>
         <S.ExplorePlansCard>
           <S.MyDataPlansCardHeader>
